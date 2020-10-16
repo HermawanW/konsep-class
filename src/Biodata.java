dfae59a897332a1db0408f12e72397972c206041
DESKTOP-2OK992U\USER <hermawanwicakasono003@gmail.com>
2020-10-16 10:09:53 +0700

    no message

---
+++ src/Biodata.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author USER
+ */
+public class Biodata {
+    public void Nama() {
+        System.out.println("Nama : Hermawan Wicaksono");
+    };
+    public void Ttl() {
+        System.out.println("Tempat, tanggal lahir : Pasuruan, 27 Februari 2003");
+    };
+    public void Alamat() {
+        System.out.println("Alamat : Jl KHA Dahlan Gg36 No21 RT 11 RW 5");
+    };
+    public void Hobby() {
+        System.out.println("Hobby : ngerusuh hobby orang lain");
+    };
+}
